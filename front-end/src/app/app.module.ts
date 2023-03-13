a38cd64d7bbcc026f9db967137f67a0946610d37
CarlaWagschal1 <91202458+CarlaWagschal1@users.noreply.github.com>
2023-03-13 11:23:39 +0100

    creationi quiz

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CreerQuizComponent } from './creer-quiz/creer-quiz.component';
+import { CreerQuizComponent } from './Quiz/creer-quiz/creer-quiz.component';
 
 @NgModule({
   declarations: [
