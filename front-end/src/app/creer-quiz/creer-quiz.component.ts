a865a94ed2a6e5f36fc8377365dd79536898ffd0
CarlaWagschal1 <91202458+CarlaWagschal1@users.noreply.github.com>
2023-03-13 10:47:51 +0100

    creation composant

---
+++ front-end/src/app/creer-quiz/creer-quiz.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-creer-quiz',
+  templateUrl: './creer-quiz.component.html',
+  styleUrls: ['./creer-quiz.component.scss']
+})
+export class CreerQuizComponent {
+
+}
