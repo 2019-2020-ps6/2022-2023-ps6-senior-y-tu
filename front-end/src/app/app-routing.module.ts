2a6511d22e07c5725f2fd3838b7a9038997e2f46
amandinemart1 <amandine.martin@etu.unic-cotedazur.fr>
2023-03-19 11:09:12 +0100

    affichage quiz et listequiz

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {QuizListComponent} from "./Quiz/quiz-list/quiz-list.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'quiz-list', component: QuizListComponent},
+  { path: '', redirectTo: '/quiz-list', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -4,15 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CreerQuizComponent } from './Quiz/creer-quiz/creer-quiz.component';
+import { QuizListComponent} from "./Quiz/quiz-list/quiz-list.component";
+import { QuizComponent} from "./Quiz/quiz/quiz.component";
+import {HttpClientModule} from "@angular/common/http";
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent,
-    CreerQuizComponent
+    CreerQuizComponent,
+    QuizComponent,
+    QuizListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    ReactiveFormsModule,
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
