6196080007afc4be836ec93cf58c3eeaabd2ccc2
ClervieCauser <clervie.causer@etu.unice.fr>
2023-06-12 17:06:59 +0200

    ajout nouvelle route config e2e + verification du clique de creation

--- front-end/src/e2e/e2e.config.ts
+++ front-end/src/e2e/e2e.config.ts
@@ -1,2 +1,4 @@
 export const accueil = 'http://localhost:4200';
 
+export const quizsErgo = 'http://localhost:4200/mes-quizs';
+
--- front-end/src/e2e/scenarios/creation-quiz-ergo.spec.ts
+++ front-end/src/e2e/scenarios/creation-quiz-ergo.spec.ts
@@ -1,16 +1,25 @@
 import  { test, expect } from '@playwright/test';
 import {QuizFormFixture} from "../../app/quizs/creer-quiz/creer-quiz.fixture";
-
+import {quizsErgo} from "../e2e.config";
 
 // This file is here to test the playwright integration
 test.describe('Creation quiz', () => {
+
+  test('Accède à la page des quizs', async ({page}) => {
+    await page.goto(quizsErgo);
+
+    await expect(page).toHaveURL(quizsErgo);
+  });
+
   test('Création quiz', async  ({ page }) => {
-    await page.goto("http://localhost:4200/mes-quizs");
+    await page.goto(quizsErgo);
 
     const quizFormFixture = new QuizFormFixture(page);
 
     const boutonCreationQuiz = await page.locator('button.creation-quiz');
     await boutonCreationQuiz.evaluateHandle((element: { click: () => any; }) => element.click())
+
+    await expect(page).toHaveURL('http://localhost:4200/creer-quiz');
 
     await test.step(`Quiz form visible`, async () => {
 
